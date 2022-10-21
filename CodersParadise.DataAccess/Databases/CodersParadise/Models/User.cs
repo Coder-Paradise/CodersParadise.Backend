d54bc339ec09b97f385bf5979b2b356f6930b21a
Aret Mimaryan <amimaryan@gmail.com>
2022-10-20 20:44:51 -0400

    Add project files.

---
+++ CodersParadise.DataAccess/Databases/CodersParadise/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace CodersParadise.DataAccess.Databases.CodersParadise.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public byte[] PaswordHash { get; set; } = new byte[32];
+        public byte[] PasswordSalt { get; set; } = new byte[32];
+        public string? VerificationToken { get; set; }
+        public DateTime? VerifiedDate { get; set; }
+        public string? PasswordResetToken { get; set; }
+        public DateTime? TokenExpiry { get; set; }
+    }
+}
