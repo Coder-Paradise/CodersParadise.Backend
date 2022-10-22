6af3babd7cfb443296418064bf8fe81968825ef2
Aret Mimaryan <amimaryan@gmail.com>
2022-10-22 12:27:36 -0400

    Set up DBContext and DI

---
+++ CodersParadise.DataAccess/Databases/CodersParadise/CodersParadiseDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CodersParadise.DataAccess.Databases.CodersParadise.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodersParadise.DataAccess.Databases.CodersParadise
+{
+    public class CodersParadiseDbContext : DbContext
+    {
+        public CodersParadiseDbContext(DbContextOptions<CodersParadiseDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Name=CodersParadiseConnection");
+        }
+
+        public DbSet<User> Users => Set<User>();
+
+
+    }
+}
---
+++ CodersParadise.DataAccess/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using CodersParadise.DataAccess.Databases.CodersParadise;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodersParadise.DataAccess
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDataAccessServices(this IServiceCollection services, IConfiguration config)
+        {
+            //Add Database Dependencies
+            services.AddDbContext<CodersParadiseDbContext>(opt => opt.UseSqlServer(config["ConnectionStrings:CodersParadiseConnection"]));
+
+            return services;
+        }
+    }
+}
