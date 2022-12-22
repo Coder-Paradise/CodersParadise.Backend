f7d2770bbe975a1459f4a12d5b4323e06e8e71a2
Aret Mimaryan <amimaryan@gmail.com>
2022-12-22 14:45:18 -0500

    Correct Column name for PasswordHash;

--- CodersParadise.DataAccess/Migrations/20221114051655_CorrectColumnName.Designer.cs
+++ CodersParadise.DataAccess/Migrations/20221114051655_CorrectColumnName.Designer.cs
@@ -4,6 +4,7 @@ using CodersParadise.DataAccess.Databases.CodersParadise;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CodersParadise.DataAccess.Migrations
 {
     [DbContext(typeof(CodersParadiseDbContext))]
-    partial class CodersParadiseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221114051655_CorrectColumnName")]
+    partial class CorrectColumnName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
